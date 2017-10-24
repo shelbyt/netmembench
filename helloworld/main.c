9bca2564d283babb150e197c07c2f55c043cc417
shelbyt <sht005@eng.ucsd.edu>
2017-10-24 16:38:17 -0700

    aesthetics

--- helloworld/main.c
+++ helloworld/main.c
@@ -62,50 +62,51 @@
 #define BURST_SIZE 32
 
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int ret;
-	unsigned lcore_id;
+    int ret;
+    int i;
+    int r_int; 
+    unsigned lcore_id;
     struct rte_mempool *mbuf_pool;
+    struct rte_mbuf *bufs[BURST_SIZE];
+
     srand(time(NULL));
 
-    // Using chars because guarenteed to be 1 byte but int can be 16 or 32 (arch)
+    // Using chars because guarenteed to be 1 byte 
     char *data; 
-    // Allocate 1024*1024-> 1mb * 50 which is larger than cache size
+    // Allocate 1024*1024-> 1mb * 500 which is larger than cache size
     int bytes = (1024*1024*500);
-
     data = (char *) malloc(bytes);
 
-	ret = rte_eal_init(argc, argv);
-	if (ret < 0)
-		rte_panic("Cannot init EAL\n");
+    ret = rte_eal_init(argc, argv);
+    if (ret < 0) 
+        rte_panic("Cannot init EAL\n");
 
- /* Creates a new mempool in memory to hold the mbufs. */
+    /* Creates a new mempool in memory to hold the mbufs. */
     mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS,
-                MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
-       struct rte_mbuf *bufs[BURST_SIZE];
+            MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, 
+            rte_socket_id());
 
-    int i;
     uint64_t start = rte_get_tsc_cycles();
-    int r_int; 
-
     for(i=0; i < 10000000; i++) {
-    rte_pktmbuf_alloc_bulk(mbuf_pool, bufs, BURST_SIZE);
-    r_int = rand() % 400000000;
-    //data[r_int] = 'c';
-    r_int = rand() % 400000000;
-    //data[r_int] = 'c';
-    r_int = rand() % 400000000;
-    //data[r_int] = 'c';
-    r_int = rand() % 400000000;
-    //data[r_int] = 'c';
-    //r_int = rand() % 400000000;
-    //data[r_int] = 'c';
-    rte_pktmbuf_free(*bufs);
+        rte_pktmbuf_alloc_bulk(mbuf_pool, bufs, BURST_SIZE);
+        //r_int = rand() % 400000000;
+        //data[r_int] = 'c';
+        //r_int = rand() % 400000000;
+        //data[r_int] = 'c';
+        //r_int = rand() % 400000000;
+        //data[r_int] = 'c';
+        //r_int = rand() % 400000000;
+        //data[r_int] = 'c';
+        //r_int = rand() % 400000000;
+        //data[r_int] = 'c';
+        rte_pktmbuf_free(*bufs);
     }
     uint64_t end = rte_get_tsc_cycles();
-    printf("lcore %d:Set Cost %" PRIu64 " cycles, and in %lf sec\n", 0, end-start, ((double)(end-start))/rte_get_tsc_hz());
 
-	return 0;
+    printf("lcore %d:Set Cost %" PRIu64 " cycles, and in %lf sec\n",
+            0, end-start, ((double)(end-start))/rte_get_tsc_hz());
+
+    return 0;
 }
